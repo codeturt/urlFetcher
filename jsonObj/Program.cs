422e623800185821de7ed654fcb2b62a6df36722
Brian Fan <brianfan@codeturtle.com>
2015-08-26 10:39:09 -0700

    Add jsonObj solution

---
+++ jsonObj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace jsonObj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var content = readFile();
+            var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+            Console.ReadLine();
+        }
+
+        static string readFile()
+        {
+            var file = File.OpenText(@"C:\Projects\_DotNet\jsonObj\jsonObj\json.txt");
+            var content = file.ReadToEnd();
+            file.Close();
+            return content;
+        }
+    }
+}
